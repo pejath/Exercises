17c92fd0dcc2b699b76cf86d0165827cababe72d
pejath <pejath@yandex.ru>
2021-10-21 00:04:36 +0300

    git add

--- task2/ex2.rb
+++ task2/ex2.rb
@@ -6,3 +6,4 @@ class Hash
     }
   end
 end
+
---
+++ task3/ex1.rb
@@ -0,0 +1,19 @@
+require '../task2/ex2'
+require 'nokogiri'
+doc = Nokogiri::XML.parse File.read('cia-1996.xml')
+p doc.root.name
+books = doc.css('country')
+population = {}
+inflation = {}
+continents = {}
+books.each { |b| population[b['name']] = b['population'].to_i }
+p 'POPULATION'
+p population.sort_by { |_key, value| value }.reverse[0]
+p 'INFLATION'
+books.each { |b| inflation[b['name']] = b['inflation'].to_i}
+p inflation.sort_by { |_key, value| value }.reverse[0, 4]
+p 'CONTINENTS'
+books.each{ |b| continents[b['name']] = b['continent']}
+
+continents = continents.safe_invert
+p continents.sort_by{ |key, value| key}
